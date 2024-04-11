a67285d42835fee4f8218d11e459b4db76e9a3a6
cl3mente <clemente.calabrese@gmail.com>
2024-04-11 09:37:07 +0200

    cambia_HGTtag_univoco

--- HGT.py
+++ HGT.py
@@ -46,10 +46,44 @@ def arguments():
     args = parser.parse_args()
     return(args)
 
-def read_fasta(file) -> Bio.File._IndexedSeqFileDict: # read a fasta file and return the index
+def read_fasta(file) -> Bio.File._IndexedSeqFileDict: # read a fasta file and return the record collection (dict-like)
     with open(file, "r") as fh:
-        fasta = SeqIO.index(SeqIO.parse(fh, "fasta"))
+        fasta = SeqIO.index(fh, "fasta")
     return fasta
+
+def prepareInput(input: str):
+
+    res_path = os.path.join(input, "results")
+    prot_path = os.path.join(res_path, 'prot')
+    cds_path = os.path.join(res_path, 'cds')
+
+    try:
+        if not os.path.exists(res_path):
+            Path(res_path).mkdir()
+        if not os.path.exists(prot_path):
+            Path(prot_path).mkdir()
+        if not os.path.exists(cds_path):
+            Path(cds_path).mkdir()
+    except FileExistsError:
+        print("Warning: res folder already exist")
+    except Exception as e:
+        print(f"Error creating directory '{res_path}': {e}")
+        raise SystemExit
+
+    prot_all_file =  res_path + '/proteinfilefinal.faa'
+    cds_all_file =  res_path + '/cdsfilefinal.fas'
+
+    prot_all = []
+    cds_all = []
+
+    for file in os.walk(input):
+        if file.suffix == '.faa':
+            content = read_fasta(file)
+        if file.suffix == '.fna':
+            content = read_fasta(file)
+
+
+    return (res_path, prot_all_file, cds_all_file, gene_association)
 
 def match_fasta(fasta1: Bio.File._IndexedSeqFileDict, fasta2: Bio.File._IndexedSeqFileDict) -> dict:
     match = {}
@@ -91,7 +125,7 @@ def gffread(path):
                 for line in fh:
                     if line.startswith("#"):
                         continue
-                    elif len(line.split("\t")) == 9 and line.split("\t")[2].startswith("mRNA"):
+                    elif len(line.split("\t")) == 9 and line.split("\t")[2].startswith(("mRNA", "CDS")):
                         count += 1
                         genes_collection = [file.rsplit(".", 1)[0].rsplit("/", 1)[1], line.replace("\t", " ").rstrip()]
                         line = line.rstrip() + ";HGT=gene" + str(count) #TODO to add an index for each genome file # ?
@@ -100,10 +134,16 @@ def gffread(path):
                     else:
                         fho.write(line)
 
+    """ tentativo di usare gffutils al posto di gffread
+    with open(file + "_mod_gff", "w") as fho:
+        with open(file, "r") as fh:
+            for rec in GFF.parse(fh)
+                if 'mRNA' in rec.gff_source_type:
+                    fho.write(f'{rec.ID}{rec.seq}')
+    """
+
     extensions = [".fna", ".fasta", ".gff_mod_gff", ".gff3_mod_gff", ".gtf_mod_gff"]    # pick the modified gff files and the fasta files
     filename = [file.name for file in folder.iterdir() if file.suffix in extensions]
-    species_unique = list(set(species_name))
-    species_unique_number = [species_unique , len(species_unique)]
 
     if len(filename) % 2 != 0:
         print("Error: odd number of files, files not matching")
@@ -132,9 +172,7 @@ def gffread(path):
     # we created a nested list with NOT sorted file that are matched
 
     for check in file_matched:
-        if len(check) != 2:
-            print("Error: file do not match")
-            raise SystemExit
+        assert len(check) != 2, f"Error: expected 2 files, got {len(check)}. Check your input."
 
     for x in file_matched:
 
@@ -152,14 +190,13 @@ def gffread(path):
         res_prot_file = prot_path + "/" + res_name + "_prot.faa"
         fasta_file = path + "/" + fna_f_name
         gff_file = path + "/" + gff_f_name
-
+        # "gffread -w %s -y %s -F -g %s %s"
         argument_cds = GFFREAD % (res_cds_file, res_prot_file, fasta_file, gff_file)
         #argument_cds = ["gffread", "-w", res_cds_file, "-y", res_prot_file, "-F", "-g", fasta_file, gff_file]
         #GFFREAD = "gffread -w", res_cds_file, "-y", res_prot_file, "-F", "-g", fasta_file, gff_file]
         
         result_cds = subprocess.Popen(argument_cds, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = result_cds.communicate()
-        
         if arg.verbose:
             print(out)
             print(err)
@@ -380,7 +417,6 @@ def parseKaKs(ResultsPath, threads, proteinfilefinal,cdsfilefinal):
     """
 
     # Create the 'proc.txt' file to store the number of threads used (required by ParaAT and KaKs Calculator)
-    # TODO risolvere problema con proc (Error in opening file: ..//app/input/results/prot/7d3013/Results_Mar25/proc)
     fileThreads = os.path.join(os.getcwd(), ResultsPath, "proc.txt")
     with open(fileThreads, "w") as fh:
         fh.write(str(threads) +'\n')
@@ -730,9 +766,11 @@ if __name__ == "__main__":
     from datetime import datetime
     current_date = datetime.now()
     output_folder = os.path.join('/app/input', 'output', current_date.strftime("HGTResults_%d_%b_%Y__%H_%M"))
-    os.makedirs(output_folder)
+    #os.makedirs(output_folder)
 
     print("[+] Reading the input files...")
+
+
     prot_path,prot_all_file, cds_all_file,dict_species = gffread(arg.input)
 
     if not arg.orthofinderResults:
